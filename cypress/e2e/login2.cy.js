606571426a5daf3aee143f1479d98546ef69f24f
GleGimenes <gle.gimenes@hotmail.com>
2023-10-31 15:58:02 -0300

    Alterações no teste, utilizando massa de dados

--- cypress/e2e/login2.cy.js
+++ cypress/e2e/login2.cy.js
@@ -2,7 +2,7 @@
 //O primeiro hooks é o BEFORE = cenário ou rotina que roda antes de todos os cenários.
 //O segundo hooks é o AFTER = cenário ou rotina que roda depois de todos os cenários. Exemplo: Limpar uma base de dados depois de rodar todos os testes.
 //O terceiro é o AFTEREACH = cenário ou rotina que roda depois de cada teste. Exemplo: Limpar algum dado comum depois de cada teste (não é comum o uso, porque o Cypress já limpa os dados depois de cada teste)
-
+const perfil = require("../fixtures/perfil.json") //utilizando massa de dados. Foi criado um file json com o nome perfil onde foi gerado a massa de dados para o login
 
 context('funcionalidade Login', () => {
 
@@ -20,6 +20,24 @@ context('funcionalidade Login', () => {
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('a > .hidden-xs').should('contain' , 'Welcome')
+    });
+
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('a > .hidden-xs').should('contain' , 'Welcome')
+    });
+
+    it.only('Deve gerar login com sucesso - Usando fixture', () => {
+        cy.fixture("perfil").then(dados => { //outra maneira é usando fixture. A partir da massa de dados criado no file perfil.json.
+        cy.get('#username').type(dados.usuario) // A função/método then carrega primeiro a massa de dados para depois então realizar o login.
+        cy.get('#password').type(dados.senha, {log: false}) //dados é uma variável criada para receber a massa de dados do file perfiljson.
+        cy.get('.woocommerce-form > .button').click() // {log: false} serve para ocultar a senha na execução na tela de teste.
+
+        cy.get('a > .hidden-xs').should('contain' , 'Welcome')
+        })
     });
 
     it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -12,7 +12,7 @@ describe('Funcionalidade página de produtos', () => {
             // .first()
             //.last()
             //.eq(3)
-            .contains('Grayson Crewneck Sweatshirt')
+            .contains('Abominable Hoodie')
             .click()
     });
 
@@ -20,14 +20,14 @@ describe('Funcionalidade página de produtos', () => {
         var quantidade = 3
 
         cy.get('[class="product-block grid"]')
-            .contains('Grayson Crewneck Sweatshirt').click()
+            .contains('Abominable Hoodie').click()
             cy.get('.button-variable-item-XL').click()
-            cy.get('.button-variable-item-Orange').click()
+            cy.get('.button-variable-item-Green').click()
             cy.get('.input-text').clear().type(quantidade)
             cy.get('.single_add_to_cart_button').click()
 
             cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Grayson Crewneck Sweatshirt” foram adicionados no seu carrinho')
+            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho')
 
     });
 
