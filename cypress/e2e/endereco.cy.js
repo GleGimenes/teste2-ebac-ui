ba4c62bf2c1cd348d7921d8840f8b10a2a9a19f3
GleGimenes <gle.gimenes@hotmail.com>
2023-10-31 19:48:15 -0300

    Alterações capturando objetos de pagina

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,5 @@
 
+import EnderecoPage from '../support/page-objects/endereco.page'
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
@@ -7,7 +8,9 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
 
     });
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Juquinha', 'Do Ceu', 'Casa do Chapeu', 'Nicaragua', 'Rua 1', '99', 'Sao Judas', 'Birigui', '02852545', '987546311', 'email@emailruim.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
 
     });
 });
